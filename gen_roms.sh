6bec5871c7b13074301f49462c092333bf23946c
OwO <OwOwOwOwO123@outlook.com>
2019-06-19 17:41:01 +0000

    gen_roms: revise list of default bit widths

--- gen_roms.sh
+++ gen_roms.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for twBits in 10 11 12 13 14 15 16 17 20 24; do
+for twBits in 10 12 14 17 24; do
 	D="generated/twiddle${twBits}b"
 	mkdir -p "$D"
-	for twSize in 4 8 16 32 64 128 256 512 1024 2048 4096; do
+	for twSize in 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768; do
 		if (( twSize > 32 )); then
 			./gen_twiddle_rom.py $twSize $twBits > "$D/twiddle_rom_$twSize.vhd"
 		else

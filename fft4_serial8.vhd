0a74f1d777ba687de2310ebb45f2f98ddca8cb3e
OwO <OwOwOwOwO123@outlook.com>
2019-08-28 06:47:16 +0000

    fft4_serial8: timing optimizations

--- fft4_serial8.vhd
+++ fft4_serial8.vhd
@@ -13,6 +13,9 @@ entity fft4_serial8 is
 			bitGrowth: integer := 0;
 			scale: scalingModes := SCALE_NONE;
 			round: boolean := true;
+			-- if true, use an extra adder for rounding for increased Fmax;
+			-- only applicable for random rounding; does not increase pipeline delay.
+			fastRounding: boolean := true;
 			convergentRound: boolean := false;
 			inverse: boolean := true);
 
@@ -24,25 +27,58 @@ entity fft4_serial8 is
 end entity;
 architecture ar of fft4_serial8 is
 	constant shift: integer := scalingShift(scale, 2);
-	constant bf2RoundPos: integer := iif(convergentRound, 0, shift);
+
+	-- 0: no rounding; 1: in-butterfly rounding; 2: fast rounding; 3: convergent rounding
+	constant roundingMode: integer := iif(shift=0 or round=false, 0,
+											iif(convergentRound, 3,
+												iif(fastRounding, 2, 1)));
+	-- how many bits to drop
+	constant bfRoundPos: integer := iif(roundingMode=1, shift, 0);
+
+	-- if dropping one bit, do it in bf2;
+	-- if dropping two bits, add 0/1 in bf1 and add 0/1 in bf2.
+	constant bf2RoundPos: integer := iif(bfRoundPos >= 1, 1, 0);
+	constant bf1RoundPos: integer := iif(bfRoundPos = 2, 1, 0);
 
 	signal ph1, ph2, ph3, ph4: unsigned(1 downto 0);
 	signal din1, bfInA, bfInB, bfOut, tmp1, tmp2: complex;
 	signal bf2InA, bf2InB, bf2InANext, bf2InBNext, bf2Out: complex;
-	signal bfSubtract, bf2SubtractRe, bf2SubtractIm: std_logic;
+	signal bfSubtractRe, bfSubtractIm, bf2SubtractRe, bf2SubtractIm: std_logic;
+	signal bf2SubtractTmp: std_logic;
 
-	signal roundRandRe, roundRandIm: std_logic;
-	signal roundAddRe, roundAddIm: signed(shift downto 0);
+	signal dinBias: std_logic := '0';
+	signal bf1Bias: std_logic := '0';
+	signal roundRandRe, roundRandIm: std_logic := '0';
+	signal roundAddRe, roundAddIm: signed(shift downto 0) := (others=>'0');
 	signal dout0Next: complex;
 
 	signal lfsr, lfsrNext: unsigned(6 downto 0) := "1101010";
 
+
+	-- register duplicates
+	signal ph1_dup0, ph1_dup1: unsigned(1 downto 0);
+
 	-- dedicate registers to important control signals
 	attribute keep: string;
 	attribute keep of ph1:signal is "true";
-	attribute keep of bfSubtract:signal is "true";
+	attribute keep of bfSubtractRe:signal is "true";
+	attribute keep of bfSubtractIm:signal is "true";
+	attribute keep of bf1Bias:signal is "true";
 	attribute keep of bf2SubtractRe:signal is "true";
 	attribute keep of bf2SubtractIm:signal is "true";
+	attribute keep of roundRandRe:signal is "true";
+	attribute keep of roundRandIm:signal is "true";
+	attribute keep of ph1_dup0:signal is "true";
+	attribute keep of ph1_dup1:signal is "true";
+
+	attribute max_fanout: integer;
+	attribute max_fanout of bfSubtractRe : signal is 16;
+	attribute max_fanout of bfSubtractIm : signal is 16;
+	attribute max_fanout of bf1Bias : signal is 16;
+	attribute max_fanout of bf2SubtractRe : signal is 16;
+	attribute max_fanout of bf2SubtractIm : signal is 16;
+	attribute max_fanout of roundRandRe : signal is 16;
+	attribute max_fanout of roundRandIm : signal is 16;
 
 	-- hack to deal with 'U' tainting the lfsr and tainting all outputs
 	function ignoreUninitialized(din: unsigned) return unsigned is
@@ -56,44 +92,81 @@ architecture ar of fft4_serial8 is
 		return ret;
 	end function;
 begin
+
+
+
 	--ph <= phase+1 when rising_edge(clk);
 	ph1 <= phase when rising_edge(clk);
 	ph2 <= ph1 when rising_edge(clk);
 	ph3 <= ph2 when rising_edge(clk);
 	ph4 <= ph3 when rising_edge(clk);
 
+	ph1_dup0 <= phase when rising_edge(clk);
+	ph1_dup1 <= phase when rising_edge(clk);
+
 	din1 <= din when rising_edge(clk);
-	bfInA <= din1 when ph1(0)='0' and rising_edge(clk);
+	
 	bfInB <= din when ph1(0)='0' and rising_edge(clk);
 
-	bfSubtract <= ph1(0) when rising_edge(clk);
+	-- fast rounding
+g8: if roundingMode = 2 generate
+		dinBias <= ph2(1) when rising_edge(clk);
+		lfsrNext <= lfsr(0) & (lfsr(6) xor lfsr(0) xor din1.re(0) xor din1.im(1)) & lfsr(5 downto 1);
+		lfsr <= ignoreUninitialized(lfsrNext) when rising_edge(clk);
+
+		roundRandRe <= lfsr(0) when rising_edge(clk);
+		roundRandIm <= lfsr(1) when rising_edge(clk);
+
+	gA: if shift=1 generate
+			roundAddRe <= "0" & (roundRandRe and dinBias);
+			roundAddIm <= "0" & (roundRandIm and dinBias);
+		end generate;
+	gB: if shift=2 generate
+			roundAddRe <= "000" when dinBias='0' else
+							"0" & roundRandRe & (not roundRandRe);
+			roundAddIm <= "000" when dinBias='0' else
+							"0" & roundRandIm & (not roundRandIm);
+		end generate;
+		bfInA.re <= din1.re + roundAddRe when ph1(0)='0' and rising_edge(clk);
+		bfInA.im <= din1.im + roundAddIm when ph1(0)='0' and rising_edge(clk);
+	end generate;
+g9: if roundingMode /= 2 generate
+		bfInA <= din1 when ph1(0)='0' and rising_edge(clk);
+	end generate;
+
+
+	bfSubtractRe <= ph1(0) when rising_edge(clk);
+	bfSubtractIm <= ph1(0) when rising_edge(clk);
 
 	bf1: entity fft4_serial8_bf
-		generic map(dataBits=>dataBits+1)
+		generic map(dataBits=>dataBits+1,
+					roundPos=>bf1RoundPos)
 		port map(clk=>clk,
 				dinA=>bfInA,
 				dinB=>bfInB,
-				subtractRe=>bfSubtract,
-				subtractIm=>bfSubtract,
-				roundRandRe=>'0',
-				roundRandIm=>'0',
+				subtractRe=>bfSubtractRe,
+				subtractIm=>bfSubtractIm,
+				roundRandRe=>bf1Bias,
+				roundRandIm=>bf1Bias,
 				dout=>bfOut);
 
 	tmp1 <= bfOut when ph2(1)='0' and rising_edge(clk);
 	tmp2 <= bfOut when ph1(1)='1' and rising_edge(clk);
 
-	bf2InANext <= tmp1 when ph1(1)='0' else tmp2;
-	bf2InBNext <= bfOut when ph1(1)='0' else to_complex(tmp1.im, tmp1.re);
-	bf2InA <= bf2InANext when ph1(0)='0' and rising_edge(clk);
-	bf2InB <= bf2InBNext when ph1(0)='0' and rising_edge(clk);
+	bf2InANext <= tmp1 when ph1_dup0(1)='0' else tmp2;
+	bf2InBNext <= bfOut when ph1_dup1(1)='0' else to_complex(tmp1.im, tmp1.re);
+	bf2InA <= bf2InANext when ph1_dup0(0)='0' and rising_edge(clk);
+	bf2InB <= bf2InBNext when ph1_dup1(0)='0' and rising_edge(clk);
 
+
+	bf2SubtractTmp <= phase(1) xor phase(0) when rising_edge(clk);
 g0: if inverse generate
-		bf2SubtractRe <= ph1(1) xor ph1(0) when rising_edge(clk);
-		bf2SubtractIm <= ph2(0);
+		bf2SubtractRe <= bf2SubtractTmp when rising_edge(clk);
+		bf2SubtractIm <= ph1(0) when rising_edge(clk);
 	end generate;
 g1: if not inverse generate
-		bf2SubtractRe <= ph2(0);
-		bf2SubtractIm <= ph1(1) xor ph1(0) when rising_edge(clk);
+		bf2SubtractRe <= ph1(0) when rising_edge(clk);
+		bf2SubtractIm <= bf2SubtractTmp when rising_edge(clk);
 	end generate;
 
 	bf2: entity fft4_serial8_bf
@@ -107,18 +180,28 @@ g1: if not inverse generate
 				roundRandIm=>roundRandIm,
 				dout=>bf2Out);
 
-g2: if not convergentRound generate
+	-- in-butterfly rounding
+g2: if roundingMode = 1 generate
 		lfsrNext <= lfsr(0) & (lfsr(6) xor lfsr(0) xor tmp1.re(0) xor tmp1.im(1)) & lfsr(5 downto 1);
 		lfsr <= ignoreUninitialized(lfsrNext) when rising_edge(clk);
 
-		roundRandRe <= lfsr(0);
-		roundRandIm <= lfsr(1);
+		roundRandRe <= lfsr(0) when rising_edge(clk);
+		roundRandIm <= lfsr(1) when rising_edge(clk);
+		bf1Bias <= ph3(1) when rising_edge(clk);
 		--roundRandRe <= bf2InA.re(1) xor bf2InA.re(2) xor bf2InB.re(1) xor bf2InB.re(2) when rising_edge(clk);
 		--roundRandIm <= bf2InA.im(1) xor bf2InA.im(2) xor bf2InB.im(1) xor bf2InB.im(2) when rising_edge(clk);
+		
+	end generate;
+
+	-- unless convergent rounding, take the output from bf2
+gOut:
+	if roundingMode /= 3 generate
 		dout <= keepNBits(shift_right(bf2Out, shift), dataBits + bitGrowth);
 	end generate;
 
-g3: if convergentRound generate
+	-- convergent rounding
+g3: if roundingMode = 3 generate
+		assert fastRounding = false;
 		-- roundRandRe/roundRandIm selects whether to round with a bias towards +inf or -inf.
 		-- in convergent rounding mode we alternate between +inf and -inf bias.
 		roundRandRe <= bf2Out.re(shift);
--- fft4_serial8_bf.vhd
+++ fft4_serial8_bf.vhd
@@ -32,16 +32,26 @@ g0: if roundPos=0 generate
 		tmp.im <= dinA.im + dinB.im when subtractIm='0' else dinA.im - dinB.im;
 	end generate;
 g1: if roundPos=1 generate
+		--tmp.re <= dinA.re + dinB.re + signed'('0' & roundRandRe) when subtractRe='0' else
+		--			dinA.re - dinB.re + signed'('0' & roundRandRe);
+		--tmp.im <= dinA.im + dinB.im + signed'('0' & roundRandIm) when subtractIm='0' else
+		--			dinA.im - dinB.im + signed'('0' & roundRandIm);
 		tmp.re <= dinA.re + operandB.re + signed'('0' & subtractRe) + signed'('0' & roundRandRe);
 		tmp.im <= dinA.im + operandB.im + signed'('0' & subtractIm) + signed'('0' & roundRandIm);
 	end generate;
 g2: if roundPos=2 generate
-		roundAddRe <= "0" & roundRandRe & (not roundRandRe);
-		roundAddIm <= "0" & roundRandIm & (not roundRandIm);
-		tmp.re <= dinA.re + dinB.re + roundAddRe when subtractRe='0' else
-					dinA.re - dinB.re + roundAddRe;
-		tmp.im <= dinA.im + dinB.im + roundAddIm when subtractIm='0' else
-					dinA.im - dinB.im + roundAddIm;
+--		roundAddRe <= "001" when roundRandRe='0' and subtractRe='0' else
+--						"010" when roundRandRe='0' else
+--						"010" when subtractRe='0' else
+--						"011";
+--		roundAddIm <= "001" when roundRandIm='0' and subtractIm='0' else
+--						"010" when roundRandIm='0' else
+--						"010" when subtractIm='0' else
+--						"011";
+		roundAddRe <= "0" & (roundRandRe or subtractRe) & (not (roundRandRe xor subtractRe));
+		roundAddIm <= "0" & (roundRandIm or subtractIm) & (not (roundRandIm xor subtractIm));
+		tmp.re <= dinA.re + operandB.re + roundAddRe;
+		tmp.im <= dinA.im + operandB.im + roundAddIm;
 	end generate;
 	dout <= keepNBits(tmp, dataBits) when rising_edge(clk);
 end a;
